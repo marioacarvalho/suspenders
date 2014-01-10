5cdd01daf41a2cf56f0c7dda07c38f9b691bcbdb
Mason Fischer <mason@thoughtbot.com>
2014-01-10 16:36:01 -0500

    remove smpt changes

--- spec/features/suspenders_new_spec.rb
+++ spec/features/suspenders_new_spec.rb
@@ -10,29 +10,11 @@ describe 'suspending a project' do
 
     expect(IO.read(staging_file)).to eq expected_staging_file
   end
-
-  it 'configures smtp' do
-    production_file = "#{suspended_directory}/config/environments/production.rb"
-    expect(IO.read(production_file)).to include('smtp')
-  end
 end
 
 def expected_staging_file
-  <<EOF
-require_relative 'production'
-Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
-EOF
-end
-def smtp_settings
-  <<EOF
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    authentication: :plain,
-    domain: 'heroku.com',
-    password: ENV['SENDGRID_PASSWORD'],
-    port: '587',
-    user_name: ENV['SENDGRID_USERNAME']
-  }
-EOF
+  <<-EOF.strip_heredoc
+    require_relative 'production'
+    Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
+  EOF
 end
